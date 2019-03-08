a85ea4368e202b15578ef758c2134a7be6df00ea
Tim van Dijen <tvdijen@gmail.com>
2019-03-08 17:10:53 +0100

    Silence travis

--- lib/Auth/Source/Twitter.php
+++ lib/Auth/Source/Twitter.php
@@ -6,11 +6,14 @@ use Webmozart\Assert\Assert;
 
 $default = dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/oauth/libextinc/OAuth.php';
 $travis = dirname(dirname(dirname(dirname(__FILE__)))).'/vendor/simplesamlphp/simplesamlphp/modules/oauth/libextinc/OAuth.php';
+$codecov = dirname(dirname(dirname(dirname(__FILE__)))).'/simplesamlphp-module-oauth/libextinc/OAuth.php';
 
 if (file_exists($default)) {
     require_once($default);
 } else if (file_exists($travis)) {
     require_once($travis);
+} else if (file_exists($codecov)) {
+    require_once($codecov);
 } else {
     throw new \Exception("Missing dependency");
 }
